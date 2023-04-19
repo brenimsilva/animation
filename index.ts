e260cfdf5db608af94f91f82f0d947cea12be073
Breno Almeida <brenimsilva7@gmail.com>
2023-04-18 22:44:10 -0300

    [Chore]: Setting Up Project

---
+++ index.ts
@@ -0,0 +1,19 @@
+const canvas = document.getElementById("canvas1") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const mobImage = new Image();
+mobImage.src = "../assets/Vampire_Girl/Attack_1.png"
+const spriteWidth = 150;
+const spriteHeight = 150;
+
+function animate() {
+    ctx!.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    // ctx!.fillRect(50,50,100,100);
+    ctx!.drawImage(mobImage, 0*spriteWidth, 0, spriteWidth, spriteHeight, 0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    requestAnimationFrame(animate);
+}
+
+animate();
